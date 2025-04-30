dc67df46df0986bff46432d10addc822710d7920
iamSt3el <himanshudhaka9166@gmail.com>
2025-04-30 19:01:12 +0530

    add the redirect file

--- src/App.js
+++ src/App.js
@@ -1,27 +1,28 @@
+// App.js with React Router
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import FileSharing from './components/FileSharing';
 import './App.css';
 
 function App() {
   return (
-    <Router>
+    <BrowserRouter>
       <div className="App min-h-screen bg-gray-100 py-8">
         <Routes>
           {/* Home route */}
           <Route path="/" element={<FileSharing />} />
           
-          {/* Explicit send mode route */}
+          {/* Explicit send route */}
           <Route path="/send" element={<FileSharing initialMode="send" />} />
           
-          {/* Explicit receive mode route - this is what the QR code will use */}
+          {/* Explicit receive route - this handles QR code scans */}
           <Route path="/receive" element={<FileSharing initialMode="receive" />} />
           
-          {/* Redirect any other routes to home */}
+          {/* Fallback route */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
       </div>
-    </Router>
+    </BrowserRouter>
   );
 }
 
